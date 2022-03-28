6d794d891fd5e9c5adc01c3226cc72626e221c06
Oculallen <morgan.allen@me.com>
2022-03-28 21:58:31 +0100

    amongus

--- main.py
+++ main.py
@@ -39,9 +39,54 @@ test_x_scaled = normalizer(test_x, train_stats)
 
 #Lets build a model booiiiiiis
 model = Sequential()
-model.add(Dense(6, activation=tf.nn.relu, input_shape=[train_x.shape[1]])),
-model.add(Dense(16, activation=tf.nn.relu)),
+model.add(Dense(6, activation=tf.nn.relu, input_shape=[train_x.shape[1]]))
+model.add(Dense(16, activation=tf.nn.relu))
 model.add(Dense(32, activation=tf.nn.relu))
 model.add(Dense(layers[-1]))
 print("Model Built!")
 
+#Config
+model.compile(optimizer='adam',
+loss='mse',
+metrics=['mse','mae'])
+
+#Early Stop
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=50)
+
+history = model.fit(
+    x=train_x_scaled,y=train_y,
+    epochs=1000,
+    validation_split=0.2,
+    verbose=0,
+    callbacks=[early_stop, tfdocs.modeling.EpochDots()]
+)
+
+#Evaluation
+# plot_obj=tfdocs.plots.HistoryPlotter(smoothing_std=2)
+# plot_obj.plot({'Auto MPG': history}, metric="mae")
+# plt.ylim([100000, 7000000])
+# plt.ylabel('MAE [Price]')
+
+test_preds = model.predict(test_x_scaled).flatten()
+
+evaluation_plot = plt.axes(aspect='equal')
+plt.scatter(test_y, test_preds)
+plt.ylabel('Price Predicted')
+plt.xlabel('Actual Price')
+plt.xlim([5000, 400000000])
+plt.ylim([5000, 400000000])
+plt.plot([5000, 400000000], [5000, 400000000])
+plt.show()
+
+new_house = pd.DataFrame([[
+    4,
+    6,
+    2,
+    5000,
+    6,
+    4
+]], columns=column_names[0:-1])
+print(new_house)
+
+new_house_price = model.predict(new_house).flatten()
+print(f'New price is: {new_house_price}')
